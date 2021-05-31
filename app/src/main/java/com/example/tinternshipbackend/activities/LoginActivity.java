f6a71524d2b018195f9a3401ab498a27e3545739
lukasberk <lukasvdberk@gmail.com>
2021-05-31 14:52:51 +0200

    added basic login layout

---
+++ app/src/main/java/com/example/tinternshipbackend/activities/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.tinternshipbackend.activities;
+
+import android.os.Bundle;
+
+import com.example.tinternshipbackend.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.tinternshipbackend.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+}
