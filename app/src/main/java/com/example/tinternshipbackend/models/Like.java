b35a1a0322a81a7a8b6541014b50b814a7f9207a
Nils van der Velden <nilsepils123@gmail.com>
2021-06-19 14:18:54 +0200

    Like model

--- app/src/main/java/com/example/tinternshipbackend/models/Like.java
+++ app/src/main/java/com/example/tinternshipbackend/models/Like.java
@@ -1,5 +1,54 @@
-//package com.example.tinternshipbackend.models;
-//
-//public class Like {
-//    String
-//}
+package com.example.tinternshipbackend.models;
+
+public class Like {
+    String id;
+    String fromUserId;
+    String toUserId;
+    boolean hasLiked;
+
+    public Like(String fromUserId, String toUserId, boolean hasLiked) {
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+        this.hasLiked = hasLiked;
+    }
+
+    public Like(String id, String fromUserId, String toUserId, boolean hasLiked) {
+        this.id = id;
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+        this.hasLiked = hasLiked;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(String fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public String getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(String toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public boolean getHasLiked() {
+        return hasLiked;
+    }
+
+    public void setHasLiked(boolean hasLiked) {
+        this.hasLiked = hasLiked;
+    }
+
+}
