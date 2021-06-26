3541c30cea269f9aeea25527211dd8fe27e1fda8
Nils van der Velden <nilsepils123@gmail.com>
2021-06-26 12:19:17 +0200

    Tried making nav bar...

--- app/src/main/java/com/example/tinternshipbackend/activities/MainActivity.java
+++ app/src/main/java/com/example/tinternshipbackend/activities/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.tinternshipbackend.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.tinternshipbackend.HomeFragment;
+import com.example.tinternshipbackend.LikesFragment;
+import com.example.tinternshipbackend.MatchesFragment;
 import com.example.tinternshipbackend.R;
 import com.example.tinternshipbackend.activities.authentication.RegisterActivity;
 import com.example.tinternshipbackend.controllers.authentication.AuthController;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
 
         AuthController authController = new AuthController(this);
 
@@ -26,4 +41,30 @@ public class MainActivity extends AppCompatActivity {
             // TODO redirect to home page if authenticated or something.
         }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            Fragment fragment = null;
+            switch (menuItem.getItemId())
+            {
+                case R.id.matches:
+                    fragment = new MatchesFragment();
+                    break;
+
+                case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+
+                case R.id.like:
+                    fragment = new LikesFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+            return true;
+        }
+    };
 }
