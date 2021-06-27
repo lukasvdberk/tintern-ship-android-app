0cf0aa92b6ac028e54ec9201a595ec6f98c61d4c
Nils van der Velden <nilsepils123@gmail.com>
2021-06-27 12:43:45 +0200

    Working on fectching all likes and then fetching all belonging companies

--- app/src/main/java/com/example/tinternshipbackend/activities/like/LikedByCompanyActivity.java
+++ app/src/main/java/com/example/tinternshipbackend/activities/like/LikedByCompanyActivity.java
@@ -1,7 +1,10 @@
 package com.example.tinternshipbackend.activities.like;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,8 +13,8 @@ import com.example.tinternshipbackend.controllers.user.UserController;
 import com.example.tinternshipbackend.databinding.ActivityLikesBinding;
 import com.example.tinternshipbackend.models.Like;
 import com.example.tinternshipbackend.models.User;
-import com.example.tinternshipbackend.models.company.CompanyProject;
-import com.example.tinternshipbackend.services.httpBackendCommunicator.HttpClient;
+
+import com.example.tinternshipbackend.models.company.Company;
 import com.example.tinternshipbackend.services.httpBackendCommunicator.HttpResponse;
 import com.example.tinternshipbackend.viewUtil.ToastUtil;
 
@@ -21,11 +24,13 @@ import java.util.List;
 public class LikedByCompanyActivity extends AppCompatActivity {
 
     ActivityLikesBinding binding;
+
     private LikeController likeController;
     private UserController userController;
     private Context mContext;
     private User user;
     private List<Like> listOfLikes = new ArrayList<>();
+    private List<Company> companiesWhoLikedMe = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,24 @@ public class LikedByCompanyActivity extends AppCompatActivity {
 
         binding = ActivityLikesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ListAdapter listAdapter = new ListAdapter(LikedByCompanyActivity.this, companiesWhoLikedMe);
+
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(LikedByCompanyActivity.this,LikeCompanyActivity.class);
+                i.putExtra("name",companiesWhoLikedMe.get(position).getName();
+                i.putExtra("phoneNumber",companiesWhoLikedMe.get(position).getPhoneNumber();
+                i.putExtra("description",companiesWhoLikedMe.get(position).getDescription();
+                startActivity(i);
+
+            }
+        });
     }
 
     private void getLikesFromUser() {
@@ -64,6 +87,7 @@ public class LikedByCompanyActivity extends AppCompatActivity {
             public void onSuccess(User data) {
                 user = data;
                 ToastUtil.showLongToast(mContext, "Success, fetched me");
+                getLikesFromUser();
             }
 
             @Override
